48047756d0f4c0132cc925d4c0b963904174a050
undym <undym1@gmail.com>
2019-09-05 12:47:36 +0900

    rsm

--- RSM/src/savedata.ts
+++ RSM/src/savedata.ts
@@ -13,7 +13,7 @@ import { Mix } from "./mix.js";
 
 
 export class Version{
-    static readonly NOW = new Version(0,7,0);
+    static readonly NOW = new Version(0,8,0);
 
     private values:number[];
 
